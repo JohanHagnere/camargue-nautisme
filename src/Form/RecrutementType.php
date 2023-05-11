91d16a4736c43344a0abcb04ee9c711000007dd4
florianC47 <florian.catusse@epsi.fr>
2023-05-11 09:47:41 +0200

    equipe

--- src/Form/RecrutementType.php
+++ src/Form/RecrutementType.php
@@ -6,21 +6,51 @@ use App\Entity\Recrutement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 class RecrutementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('age')
-            ->add('mail')
-            ->add('telephone')
-            ->add('adresse')
-            ->add('posteCandidate')
-            ->add('diplomes')
-            ->add('experiences')
+            ->add('nom',TextType::class, [ 'attr' => [
+                    'class' => 'form-control'
+                    ]
+                    ])
+               
+            ->add('prenom',TextType::class, [ 'attr' => [
+                'class' => 'form-control'
+                ]
+                ])
+            ->add('age',NumberType::class, [ 'attr' => [
+                'class' => 'form-control'
+                ]
+                ])
+            ->add('mail',TextType::class, [ 'attr' => [
+                'class' => 'form-control'
+                ]
+                ])
+            ->add('telephone',TextType::class, [ 'attr' => [
+                'class' => 'form-control'
+                ]
+                ])
+            ->add('adresse',TextareaType::class, [ 'attr' => [
+                'class' => 'form-control'
+                ]
+                ])
+            ->add('posteCandidate',TextType::class, [ 'attr' => [
+                'class' => 'form-controlb'
+                ]
+                ])
+            ->add('diplomes',TextareaType::class, [ 'attr' => [
+                'class' => 'form-controlc'
+                ]
+                ])
+            ->add('experiences',TextareaType::class, [ 'attr' => [
+                'class' => 'form-controlc'
+                ]
+                ])
         ;
     }
 
